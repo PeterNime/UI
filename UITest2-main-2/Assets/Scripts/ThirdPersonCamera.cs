67051885ce1356d542bde8d4ca11e6ff7a409523
PeterNime <63685699+PeterNime@users.noreply.github.com>
2021-09-17 11:48:14 +1200

    UI submission
    Friday the 17th

---
+++ UITest2-main-2/Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+public class ThirdPersonCamera : MonoBehaviour {
+    private const float Y_ANGLE_MIN = 0.0f;
+    private const float Y_ANGLE_MAX = 50.0f;
+    public Transform playerPos;
+    private Transform camTransform;
+    private Camera cam;
+    private float distance = 10;
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+    private float sensitivityX = 4.0f;
+    private float sensitivityY = 5.0f;
+    private Vector3 lookatPos;
+
+    public GameObject canvas;
+    InGameMenu menu;
+
+    private void Start() {
+        camTransform = transform;
+        cam = Camera.main;
+        menu = canvas.GetComponent<InGameMenu>();
+    }
+    private void Update() {
+        if (!menu.gamePaused) {
+            currentX += Input.GetAxis("Mouse X") * sensitivityX;
+            currentY += -Input.GetAxis("Mouse Y") * sensitivityY;
+            currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+            Vector3 dir = new Vector3(0, 0, -distance);
+            Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+            camTransform.position = playerPos.position + rotation * dir;
+            lookatPos = playerPos.position;
+            lookatPos.y += 4.0f;
+            camTransform.LookAt(lookatPos);
+        }
+    }
+}
