4eb56cd2a56c85d1fa9947b3ddc94f8102216a7a
PeterNime <63685699+PeterNime@users.noreply.github.com>
2021-10-31 15:43:42 +1300

    UI
    Tiles game

---
+++ UITilesGame/Assets/MainManu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainManu : MonoBehaviour
+{
+   public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+
+}
+
